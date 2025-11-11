488320c88ea91690d0f49fb5d194c095e346040d
EdgarNovas <edgar.novas@enti.cat>
2025-11-11 17:21:33 +0100

    Piece falling and stacking

---
+++ js/LevelGame.js
@@ -0,0 +1,37 @@
+class LevelGame extends Phaser.Scene {
+    constructor() {
+        super({ key: 'LevelGame' });
+    }
+
+    preload() {
+        this.load.setPath('/assets/sprites/');
+        this.load.image('red', 'Red.png');
+        this.load.image('green', 'Green.png');
+        this.load.image('blue', 'Blue.png');
+    }
+
+    create() {
+        this.grid = new Grid(this, 7, 16, 32, 200, 50);
+        this.spawnNewPiece();
+    }
+
+    spawnNewPiece() {
+        this.currentPiece = new Piece(this, this.grid, 3);
+    }
+
+    update(time, delta) {
+        if (this.currentPiece) {
+            this.currentPiece.update(time, delta);
+        }
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    backgroundColor: '#222',
+    scene: [Level1]
+};
+
+new Phaser.Game(config);
--- js/main.js
+++ js/main.js
@@ -9,10 +9,7 @@ var config = {
   width: gamePrefs.gameWidth,
   height: gamePrefs.gameHeight,
   render: { pixelArt: true },
-  physics: {
-    default: 'arcade',
-    arcade: { gravity: { y: gamePrefs.GRAVITY }, debug: true }
-  },
+  
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
